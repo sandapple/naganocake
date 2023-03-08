62891671018ca4b7eb62d251f24ccf83e3550e8c
EC2 Default User <ec2-user@ip-172-31-3-93.ap-northeast-1.compute.internal>
2023-03-08 08:20:21 +0000

    naganocake 3/8

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
- # 顧客用
+  get 'customers/index'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/update'
+  namespace :admin do
+    post 'items' => 'items#create'
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+  end
+# 顧客用
 # URL /customers/sign_in ...
-devise_for :customers, controllers: {
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-# 管理者用
-# URL /admin/sign_in ...
+#devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  #sessions: "admin/sessions"
+#}
 devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
